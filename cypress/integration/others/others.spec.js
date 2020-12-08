b1338bfa3b46ce6e9bb4eede7df1e4415eabeff6
Jakub Mareš <kubzej8@gmail.com>
2020-12-08 16:30:36 +0100

    test using destructuring

--- cypress/integration/others/others.spec.js
+++ cypress/integration/others/others.spec.js
@@ -1,5 +1,6 @@
 describe('other tests', () => {
     beforeEach(() => {
+        cy.cleanDB()
         cy.visit('/')
     })
 
@@ -14,5 +15,14 @@ describe('other tests', () => {
 
     it('it tries to to get item with 10s timeout', () => {
         cy.get('XXX', {timeout: 10000})
+    })
+
+    it('using destructuring', () => {
+        cy.request('POST', '/api/boards', {"name":"XXX Board"}).as('req')
+        cy.get('@req').then( ({ statusText, status, body }) =>  {
+            expect(statusText).to.equal('Created')
+            expect(status).to.equal(201)
+            expect(body.name).to.equal('XXX Board')
+        })
     })
 })
